97acd13f6fc37c4fc32e2b55b9844c64efd3ee03
Anton Angelov <angelov.anton@yahoo.com>
2016-03-20 22:57:17 +0200

    Update WebDriver NuGets add new demos for 'Design Grid Control Automated Tests Part 1' article http://automatetheplanet.com/design-grid-control-automated-tests-part-1/

---
+++ WebDriver-Series/WebDriver.Series.Tests/GridTestCases/Pages/GridFilterPage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace WebDriver.Series.Tests.GridTestCases.Pages
+{
+    public class GridFilterPage
+    {
+        public readonly string Url = @"http://demos.telerik.com/kendo-ui/grid/filter-row";
+        private readonly IWebDriver driver;
+
+        public GridFilterPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='grid']/div[3]/span")]
+        public IWebElement PagerInfoLabel { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='grid']/div[3]/a[3]")]
+        public IWebElement GoToNextPage { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='grid']/div[3]/a[1]")]
+        public IWebElement GoToFirstPageButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='grid']/div[3]/a[4]/span")]
+        public IWebElement GoToLastPage { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='grid']/div[3]/a[2]/span")]
+        public IWebElement GoToPreviousPage { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='grid']/div[3]/ul/li[2]/a")]
+        public IWebElement PageOnFirstPositionButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='grid']/div[3]/ul/li[3]/a")]
+        public IWebElement PageOnSecondPositionButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='grid']/div[3]/ul/li[11]/a")]
+        public IWebElement PageOnTenthPositionButton { get; set; }
+        
+        public void NavigateTo()
+        {
+            this.driver.Navigate().GoToUrl(this.Url);
+        }
+    }
+}
---
+++ WebDriver-Series/WebDriver.Series.Tests/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace WebDriver.Series.Tests
+{
+    public class Order
+    {
+        public Order(string shipName) : this()
+        {
+            if (!string.IsNullOrEmpty(shipName))
+            {
+                this.ShipName = shipName;
+            }            
+        }
+
+        public Order()
+        {
+            Random rand = new Random();
+            this.OrderId = rand.Next();
+            this.ShipName = Guid.NewGuid().ToString();
+            this.Freight = rand.Next();
+            this.OrderDate = DateTime.Now;
+        }
+
+        public int OrderId { get; set; }
+        public string ShipName { get; set; }
+        public double Freight { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
