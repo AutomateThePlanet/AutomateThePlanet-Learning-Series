2c97c31da3db8f4404777a10cf1ec24c63c10a17
Anton Angelov <angelov.anton@yahoo.com>
2016-04-18 23:10:29 +0300

    add copyright headers Apache License v 2.0

--- TestCaseManager/AAngelov.Utilities/AAngelov.Utilities/Entities/ListStackExtensions.cs
+++ TestCaseManager/AAngelov.Utilities/AAngelov.Utilities/Entities/ListStackExtensions.cs
@@ -1,8 +1,16 @@
-// <copyright file="ListStackExtensions.cs" company="CodeProject">
-// http://www.codeproject.com/Articles/456591/Simple-Undo-redo-library-for-Csharp-NET?msg=4572235#xx4572235xx All rights reserved.
+// <copyright file="ListStackExtensions.cs" company="Automate The Planet Ltd.">
+// Copyright 2016 Automate The Planet Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 // </copyright>
-// <author>Y Sujan</author>
-
+// <author>Anton Angelov</author>
+// <site>http://automatetheplanet.com/</site>
 namespace AAngelov.Utilities.Entities
 {
     using System.Collections.Generic;
