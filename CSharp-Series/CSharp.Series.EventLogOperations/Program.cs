b519b84807e8fc94e765bb975855787d9386f5f0
Anton Angelov <angelov.anton@yahoo.com>
2016-04-18 22:23:48 +0300

    add copyright header to all files in the CSharp Series

--- CSharp-Series/CSharp.Series.EventLogOperations/Program.cs
+++ CSharp-Series/CSharp.Series.EventLogOperations/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿// <copyright file="program.cs" company="Automate The Planet Ltd.">
+// Copyright 2016 Automate The Planet Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+// <author>Anton Angelov</author>
+// <site>http://automatetheplanet.com/</site>
+using System;
 using System.Diagnostics;
 
 namespace CSharp.Series.EventLogOperations
@@ -12,14 +25,14 @@ namespace CSharp.Series.EventLogOperations
             {
                 EventLog.CreateEventSource("Rock'N'Roll", "Automate The Planet");
             }
-            EventLog eventLog = new EventLog();
+            var eventLog = new EventLog();
             eventLog.Source = "Automate The Planet";
 
             // Write a new entry to the source.
             eventLog.WriteEntry("You need community. It’s here and it’s waiting just for you. ", EventLogEntryType.Information);
 
             // Reading Entries
-            EventLog automateThePlanetLog = new EventLog();
+            var automateThePlanetLog = new EventLog();
             automateThePlanetLog.Log = "Automate The Planet";
             foreach (EventLogEntry entry in automateThePlanetLog.Entries)
             {
@@ -37,7 +50,7 @@ namespace CSharp.Series.EventLogOperations
             {
                 // Delete Source
                 EventLog.Delete("Automate The Planet");
-            }           
+            }
         }
     }
 }
