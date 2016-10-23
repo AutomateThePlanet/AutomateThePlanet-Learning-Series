871bb17b0fa8e921b6bdced3f63a053adcf15427
angelov.st.anton <DUPITER_3rf3s3adio>
2016-10-23 16:38:35 +0300

    add examples Article: Getting Started with Specflow in 10 Minutes URL:  http://automatetheplanet.com/getting-started-specflow Publish Date: 23.10.2016

---
+++ Specflow-Series/GettingStartedSpecflow/Core/Driver.cs
@@ -0,0 +1,86 @@
+﻿// <copyright file="Driver.cs" company="Automate The Planet Ltd.">
+// Copyright 2016 Automate The Planet Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+// <author>Anton Angelov</author>
+// <site>http://automatetheplanet.com/</site>
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace GettingStartedSpecflow.Core
+{
+    public static class Driver
+    {
+        private static WebDriverWait browserWait;
+
+        private static IWebDriver browser;
+
+        public static IWebDriver Browser
+        {
+            get
+            {
+                if (browser == null)
+                {
+                    throw new NullReferenceException("The WebDriver browser instance was not initialized. You should first call the method Start.");
+                }
+                return browser;
+            }
+            private set
+            {
+                browser = value;
+            }
+        }
+
+        public static WebDriverWait BrowserWait
+        {
+            get
+            {
+                if (browserWait == null || browser == null)
+                {
+                    throw new NullReferenceException("The WebDriver browser wait instance was not initialized. You should first call the method Start.");
+                }
+                return browserWait;
+            }
+            private set
+            {
+                browserWait = value;
+            }
+        }
+
+        public static void StartBrowser(GettingStartedSpecflow.Core.BrowserTypes browserType = GettingStartedSpecflow.Core.BrowserTypes.Firefox, int defaultTimeOut = 30)
+        {
+            switch (browserType)
+            {
+                case GettingStartedSpecflow.Core.BrowserTypes.Firefox:
+                    Driver.Browser = new FirefoxDriver();
+                    break;
+                case GettingStartedSpecflow.Core.BrowserTypes.InternetExplorer:
+                    break;
+                case GettingStartedSpecflow.Core.BrowserTypes.Chrome:
+                    Driver.Browser = new ChromeDriver();
+                    break;
+                default:
+                    break;
+            }
+            BrowserWait = new WebDriverWait(Driver.Browser, TimeSpan.FromSeconds(defaultTimeOut));
+        }
+
+        public static void StopBrowser()
+        {
+            Browser.Quit();
+            Browser = null;
+            BrowserWait = null;
+        }
+    }
+}
