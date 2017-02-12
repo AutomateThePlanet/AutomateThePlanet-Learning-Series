39226a12f48ec87adf64deedd1010a82bb9f1064
angelov.st.anton <DSATURN_3rf3s3adio>
2017-02-12 18:48:45 +0200

    Article: https://automatetheplanet.com/selenium-webdriver-ui-tests-load-testing-cloud/ URL: Use WebDriver UI Tests for Load Testing in the Cloud Publish Date: 12.02.2017

---
+++ WebDriver-Series/WebDriverCloudLoadTesting/AutomateThePlanetTest.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="AutomateThePlanetTest.cs" company="Automate The Planet Ltd.">
+// Copyright 2017 Automate The Planet Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+// <author>Anton Angelov</author>
+// <site>http://automatetheplanet.com/</site>
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using System;
+using WebDriverCloudLoadTesting.Pages.AutomateThePlanet;
+
+namespace WebDriverCloudLoadTesting
+{
+    [TestClass]
+    public class AutomateThePlanetTest
+    {
+        private IWebDriver driver;
+        public TestContext TestContext { get; set; }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            this.driver = new PhantomJSDriver();
+            this.driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.driver.Quit();
+        }
+
+        [TestMethod]
+        public void TestInTheCloud()
+        {
+            var homePage = new HomePage(this.driver);
+            this.TestContext.BeginTimer("Automate The Planet Home Page- Navigate");
+            homePage.Navigate();
+            this.TestContext.EndTimer("Automate The Planet Home Page- Navigate");
+            homePage.AssertHeadline();
+            this.TestContext.BeginTimer("Automate The Planet- Go to Blog");
+            homePage.GoToBlog();
+            var blogPage = new BlogPage(this.driver);
+            blogPage.WaitForSubscribeWidget();
+            this.TestContext.EndTimer("Automate The Planet- Go to Blog");
+            blogPage.AssertTitle();
+        }
+    }
+}
---
+++ WebDriver-Series/WebDriverCloudLoadTesting/readme-16.txt
@@ -0,0 +1,3 @@
+﻿Article: https://automatetheplanet.com/selenium-webdriver-ui-tests-load-testing-cloud/
+URL: Use WebDriver UI Tests for Load Testing in the Cloud
+Publish Date: 12.02.2017
