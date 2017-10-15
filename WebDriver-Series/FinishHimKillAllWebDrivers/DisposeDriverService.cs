39dc519005d2bc1d78d89ef4f2d9cff58c437de9
angelov.st.anton <DSATURN_3rf3s3adio>
2017-10-15 20:41:20 +0300

    Article: https://automatetheplanet.com/kill-all-webdrivers URL: Finish Him- Kill All the WebDrivers C# Code Publish Date: 15.10.2017

---
+++ WebDriver-Series/FinishHimKillAllWebDrivers/DisposeDriverService.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="DisposeDriverService.cs" company="Automate The Planet Ltd.">
+// Copyright 2017 Automate The Planet Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+// <author>Anton Angelov</author>
+// <site>https://automatetheplanet.com/</site>
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using OpenQA.Selenium;
+
+namespace FinishHimKillAllWebDrivers
+{
+    public static class DisposeDriverService
+    {
+        private static readonly List<string> _processesToCheck = new List<string>
+                                                                     { "opera", "chrome", "firefox", "ie", "gecko", "phantomjs", "edge", "microsoftwebdriver", "webdriver" };
+
+        public static DateTime? TestRunStartTime { get; set; }
+
+        public static void FinishHim(IWebDriver driver)
+        {
+            driver?.Dispose();
+            var processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                try
+                {
+                    Debug.WriteLine(process.ProcessName);
+                    if (process.StartTime > TestRunStartTime)
+                    {
+                        var shouldKill = false;
+                        foreach (var processName in _processesToCheck)
+                        {
+                            if (process.ProcessName.ToLower().Contains(processName))
+                            {
+                                shouldKill = true;
+                                break;
+                            }
+                        }
+
+                        if (shouldKill)
+                        {
+                            process.Kill();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e);
+                }
+            }
+        }
+    }
+}
