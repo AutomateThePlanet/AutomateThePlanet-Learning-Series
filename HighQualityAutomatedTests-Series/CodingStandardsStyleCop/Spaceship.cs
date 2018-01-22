be90ef00f1a80793cf12744e14e9ab7dc8fbc0ca
angelov.st.anton <DSATURN_3rf3s3adio>
2018-01-22 20:03:29 +0200

    Article: https://automatetheplanet.com/style-consistency-rules-stylecop/ URL: High-Quality Automated Tests- Enforce Style and Consistency Rules Using StyleCop Publish Date: 22.01.2017

---
+++ HighQualityAutomatedTests-Series/CodingStandardsStyleCop/Spaceship.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingStandardsStyleCop
+{
+    public class Spaceship
+    {
+        public void NavigateToSpace()
+        {
+        }
+        public bool HasFuel
+        {
+            get { return true; }
+        }
+    }
+}
---
+++ HighQualityAutomatedTests-Series/CodingStandardsStyleCop/readme-4.txt
@@ -0,0 +1,3 @@
+﻿Article: https://automatetheplanet.com/style-consistency-rules-stylecop/
+URL: High-Quality Automated Tests- Enforce Style and Consistency Rules Using StyleCop
+Publish Date: 22.01.2017
