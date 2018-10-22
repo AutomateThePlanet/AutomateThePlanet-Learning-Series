8e509d1507a01ce348bacb9123fdec6850de7c0c
Anton Angelov <angelov.anton@yahoo.com>
2018-10-22 15:34:48 +0300

    update all license agreements

--- Design-Architecture-Series/HybridTestFramework.UITests.Core.Utilities/ExceptionsAnalysis/Decorator/ExceptionAnalyzedNavigationService.cs
+++ Design-Architecture-Series/HybridTestFramework.UITests.Core.Utilities/ExceptionsAnalysis/Decorator/ExceptionAnalyzedNavigationService.cs
@@ -1,7 +1,7 @@
 ﻿using HybridTestFramework.UITests.Core.Events;
 using HybridTestFramework.UITests.Core.Utilities.ExceptionsAnalysis.Decorator.Interfaces;
 // <copyright file="ExceptionAnalyzedNavigationService.cs" company="Automate The Planet Ltd.">
-// Copyright 2016 Automate The Planet Ltd.
+// Copyright 2018 Automate The Planet Ltd.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
