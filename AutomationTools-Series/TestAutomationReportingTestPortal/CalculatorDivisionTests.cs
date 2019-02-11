455f286af126a7b89fe1ff326f664722ef281515
angelov.st.anton <angelov.anton@yahoo.com>
2019-02-11 19:30:30 +0200

    add demos for Article: Test Automation Reporting with Test Portal in .NET Projects URL:  http://automatetheplanet.com/test-automation-reporting-test-portal Publish Date: 11.02.2019

---
+++ AutomationTools-Series/TestAutomationReportingTestPortal/CalculatorDivisionTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAutomationReportingTestPortal
+{
+    [TestClass]
+    class CalculatorDivisionTests
+    {
+        [TestMethod]
+        public void Return4_WhenMultiply2And2()
+        {
+            var calculator = new Calculator();
+
+            int actualResult = calculator.Multiply(2, 2);
+
+            Assert.AreEqual(4, actualResult);
+        }
+
+        [TestMethod]
+        public void Return0_WhenMultiply0And0()
+        {
+            var calculator = new Calculator();
+
+            int actualResult = calculator.Multiply(0, 0);
+
+            Assert.AreEqual(0, actualResult);
+        }
+
+        [TestMethod]
+        public void ReturnMinus5_WhenMultiply5AndMinus1()
+        {
+            var calculator = new Calculator();
+
+            int actualResult = calculator.Multiply(5, -1);
+
+            Assert.AreEqual(0, actualResult);
+        }
+    }
+}
---
+++ AutomationTools-Series/TestAutomationReportingTestPortal/CalculatorMultipleTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAutomationReportingTestPortal
+{
+    [TestClass]
+    public class CalculatorMultipleTests
+    {
+        [TestMethod]
+        public void ThrowException_When_DivisionOn0()
+        {
+            var calculator = new Calculator();
+
+            float actualResult = calculator.Division(2, 0);
+
+            Assert.AreEqual(4, actualResult);
+        }
+
+        [TestMethod]
+        public void Return2_When_Division2On1()
+        {
+            var calculator = new Calculator();
+
+            float actualResult = calculator.Division(2, 1);
+
+            Assert.AreEqual(4, actualResult);
+        }
+    }
+}
