4d8bfcf991121fbbaf28f37396522a61e45c6fc3
angelov.st.anton <angelov.anton@yahoo.com>
2019-12-25 18:04:10 +0200

    remove licenses

--- AutomationTools-Series/BenchmarkingAutomatedTesting/DriverExecutablePathResolver.cs
+++ AutomationTools-Series/BenchmarkingAutomatedTesting/DriverExecutablePathResolver.cs
@@ -1,18 +1,4 @@
-﻿// <copyright file="DriverExecutablePathResolver.cs" company="Automate The Planet Ltd.">
-// Copyright 2020 Automate The Planet Ltd.
-// Licensed under the Royalty-free End-user License Agreement, Version 1.0 (the "License");
-// You may not use this file except in compliance with the License.
-// You may obtain a copy of the License at http://bellatrix.solutions/licensing-royalty-free/
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// </copyright>
-// <author>Anton Angelov</author>
-// <site>https://bellatrix.solutions/</site>
-
-using System.IO;
+﻿using System.IO;
 using System.Reflection;
 
 namespace BenchmarkingAutomatedTesting
