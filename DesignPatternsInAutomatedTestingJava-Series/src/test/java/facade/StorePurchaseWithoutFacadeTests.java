4596e0b0ea9f19b3ae60a1ffbb28dd3c06ae317a
Teodor Nikolov <teo8447@gmail.com>
2021-01-14 15:54:04 +0200

    Added license headers

--- DesignPatternsInAutomatedTestingJava-Series/src/test/java/facade/StorePurchaseWithoutFacadeTests.java
+++ DesignPatternsInAutomatedTestingJava-Series/src/test/java/facade/StorePurchaseWithoutFacadeTests.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2021 Automate The Planet Ltd.
+ * Author: Anton Angelov
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package facade;
 
 import facade.core.Driver;
@@ -38,7 +50,7 @@ public class StorePurchaseWithoutFacadeTests {
         purchaseInfo.setCompany("Space Flowers");
         purchaseInfo.setCountry("Germany");
         purchaseInfo.setAddress1("1 Willi Brandt Avenue Tiergarten");
-        purchaseInfo.setAddress2("Lьtzowplatz 17");
+        purchaseInfo.setAddress2("Lützowplatz 17");
         purchaseInfo.setCity("Berlin");
         purchaseInfo.setZip("10115");
         purchaseInfo.setPhone("+491888999281");
@@ -67,7 +79,7 @@ public class StorePurchaseWithoutFacadeTests {
         purchaseInfo.setCompany("Space Flowers");
         purchaseInfo.setCountry("Germany");
         purchaseInfo.setAddress1("1 Willi Brandt Avenue Tiergarten");
-        purchaseInfo.setAddress2("Lьtzowplatz 17");
+        purchaseInfo.setAddress2("Lützowplatz 17");
         purchaseInfo.setCity("Berlin");
         purchaseInfo.setZip("10115");
         purchaseInfo.setPhone("+491888999281");
