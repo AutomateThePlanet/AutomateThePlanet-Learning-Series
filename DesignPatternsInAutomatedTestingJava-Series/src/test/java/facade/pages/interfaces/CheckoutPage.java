baea3277bc5d150171eba410950a083c6311ddfb
Teodor Nikolov <teo8447@gmail.com>
2021-01-15 09:49:19 +0200

    removed unused methods from page interface

--- DesignPatternsInAutomatedTestingJava-Series/src/test/java/facade/pages/interfaces/CheckoutPage.java
+++ DesignPatternsInAutomatedTestingJava-Series/src/test/java/facade/pages/interfaces/CheckoutPage.java
@@ -15,8 +15,6 @@ package facade.pages.interfaces;
 import facade.data.PurchaseInfo;
 
 public interface CheckoutPage {
-    void navigate(String url);
-    void navigate();
     void fillBillingInfo(PurchaseInfo purchaseInfo);
     void assertSubtotal(double itemPrice);
 }
