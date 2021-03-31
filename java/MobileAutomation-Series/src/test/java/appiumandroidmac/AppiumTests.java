ea72f2eb782f8d9369777186dbb42bbb60dd1680
Teodor Nikolov <teo8447@gmail.com>
2021-03-31 03:01:08 +0300

    removed unneeded import

--- java/MobileAutomation-Series/src/test/java/appiumandroidmac/AppiumTests.java
+++ java/MobileAutomation-Series/src/test/java/appiumandroidmac/AppiumTests.java
@@ -13,7 +13,6 @@
 
 package appiumandroidmac;
 
-import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
