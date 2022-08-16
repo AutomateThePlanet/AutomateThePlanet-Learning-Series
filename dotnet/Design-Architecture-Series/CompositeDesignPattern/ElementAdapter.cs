a2a5de97132d1cfcd7ed65dd9da9f03b781845ec
Anton Angelov <aangelov@automatetheplanet.com>
2022-08-16 18:40:19 +0300

    update license agreement

--- dotnet/Design-Architecture-Series/CompositeDesignPattern/ElementAdapter.cs
+++ dotnet/Design-Architecture-Series/CompositeDesignPattern/ElementAdapter.cs
@@ -1,5 +1,5 @@
 ﻿// <copyright file="ElementAdapter.cs" company="Automate The Planet Ltd.">
-// Copyright 2021 Automate The Planet Ltd.
+// Copyright 2022 Automate The Planet Ltd.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -15,6 +15,7 @@ using System.Collections.ObjectModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using SE = SeleniumExtras.WaitHelpers;
 
 namespace CompositeDesignPattern;
